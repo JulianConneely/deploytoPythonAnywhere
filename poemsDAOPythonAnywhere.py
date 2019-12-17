a1637a02ba8ec71d347ae9771e75e244f9cf741d
JulianConneely <36239852+JulianConneely@users.noreply.github.com>
2019-12-17 16:33:30 +0000

    Create poemsDAOPythonAnywhere.py

---
+++ poemsDAOPythonAnywhere.py
@@ -0,0 +1,70 @@
+import mysql.connector
+import dbconfiguration as cfg
+class poemsDAO:
+    db=""
+    def __init__(self): 
+        self.db = mysql.connector.connect(
+        host=       cfg.mysql['host'],
+        user=       cfg.mysql['username'],
+        password=   cfg.mysql['password'],
+        database=   cfg.mysql['database']
+        )
+    
+            
+    def create(self, values):
+        cursor = self.db.cursor()
+        sql="insert into poems (title,author, price) values (%s,%s,%s)"
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        return cursor.lastrowid
+
+    def getAll(self):
+        cursor = self.db.cursor()
+        sql="select * from poems"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        print(results)
+        for result in results:
+            print(result)
+            returnArray.append(self.convertToDictionary(result))
+
+        return returnArray
+
+    def findByID(self, id):
+        cursor = self.db.cursor()
+        sql="select * from poems where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        return self.convertToDictionary(result)
+
+    def update(self, values):
+        cursor = self.db.cursor()
+        sql="update poems set title= %s,author=%s, price=%s  where id = %s"
+        cursor.execute(sql, values)
+        self.db.commit()
+    def delete(self, id):
+        cursor = self.db.cursor()
+        sql="delete from poems where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        print("delete done")
+
+    def convertToDictionary(self, result):
+        colnames=['id','Title','Author', "Price"]
+        item = {}
+        
+        if result:
+            for i, colName in enumerate(colnames):
+                value = result[i]
+                item[colName] = value
+        
+        return item
+        
+poemsDAO = poemsDAO()
